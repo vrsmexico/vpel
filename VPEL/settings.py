f28fc55703516a8bfb105412a04265797a63685b
vrsmexico <92056572+vrsmexico@users.noreply.github.com>
2022-01-03 18:08:10 -0800

    cambiando stactic

--- VPEL/settings.py
+++ VPEL/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,15 +75,15 @@ WSGI_APPLICATION = 'VPEL.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+#if DEBUG:
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': BASE_DIR / 'db.sqlite3',
+#        }
+#    }
+#else:
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'vpel',
@@ -91,7 +92,7 @@ else:
         'HOST': 'localhost',
         'PORT': '',
         }
-    }
+}
 
 
 # Password validation
@@ -129,8 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 MEDIA_URL='/media/'
-MEDIA_ROOT=BASE_DIR/'media'
+
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL='catalogo'
 
